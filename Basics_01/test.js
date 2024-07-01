17ad5bc84ac0c8072fe3fa44a303bd9a67058a38
kaminijatav <148079846+kaminijatav@users.noreply.github.com>
2024-07-01 15:21:30 +0000

    Conversion to other data types

---
+++ Basics_01/test.js
@@ -0,0 +1,19 @@
+// let score="33";
+// console.log(typeof(score));// check data type 
+// console.log(score);
+// let scoreInNumber=Number(score); 
+//convert data type in another data type
+// let scoreInBoolean=Boolean(score); 
+let isloggedIn="dfg";
+let inBoolean=Boolean(isloggedIn);
+console.log(inBoolean);
+// console.log(score);
+//convert data type in another data type
+
+
+
+//convert other data type  in Number
+// "33"=>33
+// "33abc"=>NaN
+// true=>1;false=>0
+
