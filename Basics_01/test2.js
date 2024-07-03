181e4aa5ffbb8aef6891dafc6552f95a9df47a3c
kaminijatav <148079846+kaminijatav@users.noreply.github.com>
2024-07-03 03:22:03 +0000

    number and math

---
+++ Basics_01/test2.js
@@ -0,0 +1,11 @@
+console.log("Kimmi");
+const score =45;
+console.log(score);
+const num= new Number(122);
+console.log(num);
+console.log (score.toFixed(2));
+console.log (num.toString(2));
+const num1=123.89;
+console.log(num1.toPrecision(3));
+const num2=100000;
+console.log(num2.toLocaleString('en-IN'));
