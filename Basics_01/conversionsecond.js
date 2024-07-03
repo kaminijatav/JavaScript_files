cc7371f3d86682d53b605a33ab6e1f768e3e051a
kaminijatav <148079846+kaminijatav@users.noreply.github.com>
2024-07-03 03:42:08 +0000

    number and math

---
+++ Basics_01/conversionsecond.js
@@ -0,0 +1,12 @@
+// let value=3;
+// let negvalue=-value;
+// console.log(value);
+// console.log(negvalue);
+let age=23;
+function1("KImmi");
+function1(age);
+ function function1(age){
+    console.log(age);
+}
+
+
